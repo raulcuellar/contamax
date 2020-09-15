cf1a55167dcc5c0af6578dd543cd4c635c30b37f
Raul <imac@iMac-de-Raul.local>
2020-09-14 19:05:59 -0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { InventarioComponent } from './inventario/inventario.component';
+import { IngresoComponent } from './ingreso/ingreso.component';
+import { EgresoComponent } from './egreso/egreso.component';
+import { CookieService } from 'ngx-cookie-service';
+import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    InventarioComponent,
+    IngresoComponent,
+    EgresoComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    routing,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/inventario/inventario.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from "../users.service";
+
+@Component({
+  selector: 'app-inventario',
+  templateUrl: './inventario.component.html',
+  styleUrls: ['./inventario.component.css']
+})
+export class InventarioComponent implements OnInit {
+  constructor(public userService: UsersService) {}
+  ngOnInit() {
+    this.getUserLogged();
+  }
+  getUserLogged() {
+    this.userService.getUser().subscribe(user => {
+      console.log(user);
+    });
+  }
+}
+
